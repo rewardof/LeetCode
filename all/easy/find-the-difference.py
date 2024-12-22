70cf09f9200f0c5b28a7f0f37d2d4bae7fed64fa
Tohirbek Soliyev <tohirbeksoliyev88@gmail.com>
2024-12-22 12:29:03 +0500

    solved: find-the-difference

--- all/easy/find-the-difference.py
+++ all/easy/find-the-difference.py
@@ -10,3 +10,41 @@ string s and then add one more letter at a random position.
 
 Return the letter that was added to t.
 """
+from collections import defaultdict
+
+
+class Solution:
+    def findTheDifference2(self, s: str, t: str) -> str:
+        s_dict = defaultdict(int)
+        for char in s:
+            s_dict[char] += 1
+
+        for char in t:
+            if char in s_dict and s_dict[char] > 0:
+                s_dict[char] -= 1
+            else:
+                return char
+
+    def findTheDifference3(self, s: str, t: str) -> str:
+        sum1 = 0
+        for char in s:
+            sum1 += ord(char)
+        sum2 = 0
+        for char in t:
+            sum2 += ord(char)
+
+        return chr(sum2 - sum1)
+
+    def findTheDifference4(self, s: str, t: str) -> str:
+        return chr(sum(map(ord, t)) - sum(map(ord, s)))
+
+    def findTheDifference(self, s: str, t: str) -> str:
+        result = 0
+        for char in s + t:
+            result ^= ord(char)
+
+        return chr(result)
+
+s = "a"
+t = "aa"
+print(Solution().findTheDifference(s, t))
