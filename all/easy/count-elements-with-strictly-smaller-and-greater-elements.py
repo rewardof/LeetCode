016f87f1a1d545dcab0d10f1654bd7dacd0e6912
Tohirbek Soliyev <tohirbeksoliyev88@gmail.com>
2025-02-14 14:34:38 +0500

    solved: 2148. Count Elements With Strictly Smaller and Greater Elements

--- all/easy/count-elements-with-strictly-smaller-and-greater-elements.py
+++ all/easy/count-elements-with-strictly-smaller-and-greater-elements.py
@@ -12,10 +12,10 @@ class Solution:
         return max(len(nums) - counts[nums[0]] - counts[nums[-1]], 0)
 
     def countElements2(self, nums: List[int]) -> int:
-        min_value, min_value = min(nums), max(nums)
+        min_value, max_value = min(nums), max(nums)
         counts = Counter(nums)
-        return max(len(nums) - counts[min_value] - counts[min_value], 0)
+        return max(len(nums) - counts[min_value] - counts[max_value], 0)
 
 
-nums = [-3, 3, 3, 90]
-print(Solution().countElements(nums))
+nums = [-71, -71, 93, -71, 40]
+print(Solution().countElements2(nums))
