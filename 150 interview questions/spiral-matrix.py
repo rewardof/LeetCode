75ed90f2651d50c4d37bbc3a4f012bd783412af4
Tohirbek Soliyev <tohirbeksoliyev88@gmail.com>
2025-04-08 06:52:09 +0500

    solved: 54. Spiral Matrix

---
+++ 150 interview questions/spiral-matrix.py
@@ -0,0 +1,52 @@
+"""
+54. Spiral Matrix
+"""
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        result = []
+        top = 0
+        bottom = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+            if left <= right:
+                for i in range(left, right + 1):
+                    result.append(matrix[top][i])
+                left += 1
+
+        return result
+
+
+
+
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+print(Solution().spiralOrder(matrix))
+
+"""
+[0][0]
+[0][1]
+[0][2]
+[1][2]
+[2][2]
+[2][1]
+[2][0]
+[1][0]
+[1][1]
+
+
+"""
